0745f3851df17bb8321e4738b6ad019aab54dd4e
Jatin Jasoliya <>
2021-02-02 06:53:43 -0500

    Add assets and Built header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
+import Header from "./components/Header";
+// import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Tesla</h1>
+      <Header />
     </div>
   );
 }
